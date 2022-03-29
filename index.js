d0809fa9f20cf5b65a00448ff757071e6ad29018
SuiCarrot <91708805+SuiCarrot@users.noreply.github.com>
2022-03-29 16:12:01 -0300

    1th att

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000; // Const para armanezar a porta do servidor
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
